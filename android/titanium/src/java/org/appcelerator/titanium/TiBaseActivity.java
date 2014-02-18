199113f782241b4ebed5f7836c6d357f448bb033
Jonathan Benay <jonathan.benay@shopmium.com>
2014-02-18 17:45:28 +0100

    [Shopmium, TiBaseActivity] Avoid crash into TiBaseActivity by testing null
    Into shopmium application it happens that dialogs list contains
    DialogWrapper without
    dialog. This lead to NullPointerException.

--- android/titanium/src/java/org/appcelerator/titanium/TiBaseActivity.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiBaseActivity.java
@@ -272,6 +272,10 @@ public abstract class TiBaseActivity extends FragmentActivity
 	{
 		for (int i = 0; i < dialogs.size(); i++) {
 			DialogWrapper p = dialogs.get(i);
+			if (p == null||p.getDialog() == null ) {
+					Log.d(TAG, "-------------------- Dialog object not found, skipping");
+					continue;
+			}	
 			if (p.getDialog().equals(d)) {
 				p.release();
 				dialogs.remove(i);
@@ -1402,4 +1406,3 @@ public abstract class TiBaseActivity extends FragmentActivity
 		return false;
 	}
 }
-

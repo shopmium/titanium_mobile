58c4c169bd576c23d8dc5036c35e6295bef72a14
Jonathan Benay <jonathan.benay@shopmium.com>
2014-10-07 12:24:12 +0200

    [Shopmium-android, progress bar] add progress listener for not has map data send

--- android/modules/network/src/java/ti/modules/titanium/network/TiHTTPClient.java
+++ android/modules/network/src/java/ti/modules/titanium/network/TiHTTPClient.java
@@ -1397,9 +1397,12 @@ public class TiHTTPClient
 
 	private void handleURLEncodedData(UrlEncodedFormEntity form)
 	{
+		final long dataLength;
+		long length = -1;
 		AbstractHttpEntity entity = null;
 		if (data instanceof String) {
 			try {
+				length = ((String) data).length();
 				entity = new StringEntity((String) data, "UTF-8");
 
 			} catch(Exception ex) {
@@ -1407,11 +1410,20 @@ public class TiHTTPClient
 				Log.e(TAG, "Exception, implement recovery: ", ex);
 			}
 		} else if (data instanceof AbstractHttpEntity) {
+			length = ((AbstractHttpEntity) data).getContentLength();
 			entity = (AbstractHttpEntity) data;
 		} else {
+			length = form.getContentLength();
 			entity = form;
 		}
 		
+		if (length == 0) {
+			dataLength = -1;
+		} else {
+			dataLength = length;
+		}
+
+
 		if (entity != null) {
 			Header header = request.getFirstHeader("Content-Type");
 			if(header == null) {
@@ -1421,7 +1433,15 @@ public class TiHTTPClient
 				entity.setContentType(header.getValue());
 			}
 			HttpEntityEnclosingRequest e = (HttpEntityEnclosingRequest)request;
-			e.setEntity(entity);
+
+			ProgressEntity progressEntity = new ProgressEntity(entity, new ProgressListener() {
+							public void progress(int progress) {
+								KrollDict data = new KrollDict();
+								data.put("progress", ((double)progress)/dataLength);
+								dispatchCallback("onsendstream", data);
+							}
+						});
+			e.setEntity(progressEntity);
 		}
 	}
 

552016b3b6ebe2a5829c94374b6c8375c33ac79a
Arnaud Besnier <arnaudIbesnier@gmail.com>
2015-09-15 10:29:30 +0200

    Rollback useless diff

--- android/titanium/src/java/org/appcelerator/titanium/TiApplication.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiApplication.java
@@ -431,10 +431,10 @@ public abstract class TiApplication extends Application implements KrollApplicat
 		TiConfig.DEBUG = TiConfig.LOGD = appProperties.getBool("ti.android.debug", false);
 		USE_LEGACY_WINDOW = appProperties.getBool(PROPERTY_USE_LEGACY_WINDOW, false);
 
-		// startExternalStorageMonitor();
+		startExternalStorageMonitor();
 
-		// // Register the default cache handler
-		// responseCache = new TiResponseCache(getRemoteCacheDir(), this);
+		// Register the default cache handler
+		responseCache = new TiResponseCache(getRemoteCacheDir(), this);
 		// TiResponseCache.setDefault(responseCache);
 		// KrollRuntime.setPrimaryExceptionHandler(new TiExceptionHandler());
 	}
